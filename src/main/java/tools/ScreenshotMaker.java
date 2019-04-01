b3d750a83b52e059b14441c8517ecf80145ec0e5
Yuliya Khilko <ylkhilko@gmail.com>
2019-04-02 02:44:10 +0300

    fix pom.xml for allure reports

--- src/main/java/tools/ScreenshotMaker.java
+++ src/main/java/tools/ScreenshotMaker.java
@@ -2,7 +2,6 @@ package tools;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
